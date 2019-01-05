bd76faed29f9e449af0c147f8238680420f75615
MrSpinne <spinningplays.gaming@gmail.com>
2019-01-05 18:29:28 +0100

    Create coding1.py

---
+++ coding1.py
@@ -0,0 +1,10 @@
+import requests
+
+chal_url = "https://cc.the-morpheus.de/challenges/1/"
+sol_url = "https://cc.the-morpheus.de/solutions/1/"
+
+g = requests.get(chal_url).text
+p = requests.post(sol_url, json.dumps({"token": g})).text
+
+print(g) # Print solution
+print(p) # Print code "Success: TMT{WBVjoml6PjsOu3yzFvr3}"
