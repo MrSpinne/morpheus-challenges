b29687649d21af2bcbbf5d3f66578825ad5b33cf
MrSpinne <spinningplays.gaming@gmail.com>
2019-01-05 23:51:06 +0100

    Update coding6.py

--- coding6.py
+++ coding6.py
@@ -7,12 +7,12 @@ chal_url = "https://cc.the-morpheus.de/challenges/6/"
 sol_url = "https://cc.the-morpheus.de/solutions/6/"
 
 g = int(requests.get(chal_url).text)
-bin = ""
+b = ""
 while g >= 1:
-    bin = str(int(g % 2)) + bin
+    b = str(int(g % 2)) + b # Attach every new bit calculated to beginning
     g = int(g // 2)
 
-p = requests.post(sol_url, json.dumps({"token": bin})).text
+p = requests.post(sol_url, json.dumps({"token": b})).text
 
-print(bin) # Print solution
+print(b) # Print solution
 print(p) # Print code "TMT{O6gjviTFP0f1Uv25chkI}"
