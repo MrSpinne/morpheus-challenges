e6ff608d5b10cfcf62d5488dc6109fb4ed4c13af
MrSpinne <spinningplays.gaming@gmail.com>
2019-01-05 18:40:19 +0100

    Create coding3.py

---
+++ coding3.py
@@ -0,0 +1,15 @@
+# Get the k-biggest number from a list from chal_url and post it to sol_url
+
+import requests
+import json
+
+chal_url = "https://cc.the-morpheus.de/challenges/3/"
+sol_url = "https://cc.the-morpheus.de/solutions/3/"
+
+g = requests.get(chal_url).json()
+g["list"].sort()
+num = g["list"][-g["k"]]
+p = requests.post(sol_url, json.dumps({"token": num})).text
+
+print(num) # Print solution
+print(p) # Print code "Success: TMT{S0SpkHzubAmCu7H3r5wR}"
